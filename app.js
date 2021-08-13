31faf5ef0a8c1caf2149ad93080a9cc1bafc3e8a
marville001 <mwangimartin1904@gmail.com>
2021-08-13 16:55:56 +0300

    Added Cart

--- app.js
+++ app.js
@@ -7,3 +7,13 @@ let menuBtn = getElement("fa-bars")
 menuBtn.addEventListener("click", ()=>{
     navs.classList.toggle("show")
 })
+
+let cartModal = getElement("cart")
+let cartBtn = getElement("btn-cart")
+let cartExit = getElement("btn-cart-exit")
+cartBtn.addEventListener("click", ()=>{
+    cartModal.classList.toggle("show")
+})
+cartExit.addEventListener("click", ()=>{
+    cartModal.classList.remove("show")
+})
