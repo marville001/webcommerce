977235281fe8e123c79de4c2d682dedf0beaa826
marville001 <mwangimartin1904@gmail.com>
2021-08-16 08:57:19 +0300

    Remove from cart updated

--- js/app.js
+++ js/app.js
@@ -15,19 +15,19 @@ class App {
   }
 
   loadProducts = () => {
-    const items = this.products.map((product) => {
+    let items = [];
+    items = this.products.map((product) => {
       const { id, tag, image, name, price, price_org } = product;
       return `<div class="item">
-                    <p class="tag">${tag}</p>
-                    <img src="./assets/${image}" alt="">
-                    <h3 class="name">${name}</h3>
-                    <h5 class="price">KSH ${price} ${
+                      <p class="tag">${tag}</p>
+                      <img src="./assets/${image}" alt="">
+                      <h3 class="name">${name}</h3>
+                      <h5 class="price">KSH ${price} ${
         price_org !== undefined ? ` <span> KSH ${product.price_org}</span>` : ""
       }</h5>
-                    <button data-id=${id} class="btn-add-to-cart"><i class="fa fa-cart-plus"></i>Add</button>
-            </div>`;
+                      <button data-id=${id} class="btn-add-to-cart"><i class="fa fa-cart-plus"></i>Add</button>
+              </div>`;
     });
-
     for (let item in items) {
       this.itemList.innerHTML += items[item];
     }
@@ -71,7 +71,17 @@ class App {
     const { id, image, name, price } = item;
     this.cart.push({ id, image, name, price, quantity: 1 });
     this.loadCart();
-    this.showNotification("Item Added to cart")
+    this.showNotification("Item Added to cart");
+  };
+
+  removeFromCart = (element) => {
+    const itemId = parseInt(element.dataset.id);
+    console.log({itemId});
+    const items = this.cart.filter((product) => product.id !== itemId);
+    
+    this.cart = items;
+    this.loadCart();
+    this.showNotification("Removed from cart");
   };
 
   calculateCartTotal = () => {
@@ -81,33 +91,33 @@ class App {
     const tax = Math.floor((this.tax / 100) * total);
     this.cartTotal.innerHTML = `Ksh ${total}`;
     this.cartTax.innerHTML = `Ksh ${tax}`;
-    this.summaryTotal.innerHTML = `Ksh ${total+tax}`;
+    this.summaryTotal.innerHTML = `Ksh ${total + tax}`;
   };
 
-  disableNotification = ()=>{
-      this.notification.classList.remove("show");
-      this.noticeText.innerHTML = "";
-  }
+  disableNotification = () => {
+    this.notification.classList.remove("show");
+    this.noticeText.innerHTML = "";
+  };
 
-  showNotification = (notice)=>{
-      this.noticeText.innerHTML = notice;
-      this.notification.classList.add("show");
+  showNotification = (notice) => {
+    this.noticeText.innerHTML = notice;
+    this.notification.classList.add("show");
 
-      setTimeout(()=>{
-          this.disableNotification()
-      }, 3000)
-  }
+    setTimeout(() => {
+      this.disableNotification();
+    }, 3000);
+  };
 
   decrementQuantity = (element) => {
     const itemId = parseInt(element.dataset.id);
-    let tempCart = this.cart.map(c=>{
-        if(c.id===itemId){
-            c.quantity>1 && c.quantity--;
-            return c;
-        }else{
-            return c;
-        }
-    })
+    let tempCart = this.cart.map((c) => {
+      if (c.id === itemId) {
+        c.quantity > 1 && c.quantity--;
+        return c;
+      } else {
+        return c;
+      }
+    });
 
     this.cart = tempCart;
 
@@ -115,14 +125,14 @@ class App {
   };
   incrementQuantity = (element) => {
     const itemId = parseInt(element.dataset.id);
-    let tempCart = this.cart.map(c=>{
-        if(c.id===itemId){
-            c.quantity++;
-            return c;
-        }else{
-            return c;
-        }
-    })
+    let tempCart = this.cart.map((c) => {
+      if (c.id === itemId) {
+        c.quantity++;
+        return c;
+      } else {
+        return c;
+      }
+    });
 
     this.cart = tempCart;
 
@@ -150,11 +160,11 @@ const eventListeners = () => {
 
   cartItems.addEventListener("click", (e) => {
     if (e.target.classList.contains("cart-quantity-minus")) {
-      console.log(e.target);
       app.decrementQuantity(e.target);
     } else if (e.target.classList.contains("cart-quantity-plus")) {
-      console.log(e.target);
       app.incrementQuantity(e.target);
+    } else if (e.target.classList.contains("fa-trash")){
+      app.removeFromCart(e.target);
     }
   });
 };
